af879b5c9322ae24b82a3c84ff30d4b5375ada33
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2020-05-24 15:02:02 +0900

    working 5/24PM3:00

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -4,17 +4,19 @@ $(function(){
                     <div class="input_box__upload" data-index="${index}">
                       <input class="input_box__upload__field" type="file" name="product[images_attributes][${index}][image]" id="product_images_attributes_${index}_image">
                       <i class="fas fa-camera fa-2x"></i>
+                      <input name="product[item_imgs][_destroy]" type="hidden" value="${index}">
+                      <input class="hidden-destroy" data-index="${index}" type="checkbox" value="${index}" name="product[item_imgs][_destroy]" id="product_item_imgs__destroy">
                     </div>
-                  </label>`;
+                </label>`;
     return html;
   }
   
+
   let previewImages = function(index, url) {
     let html = `<div class="previews__image">
                   <div class="previews__image__box">
                     <img data-image="{index:${index}}" src="${url}" width="100" height="100"></div>
                   <div class="previews__image__btn" data-btn-index="${index}">削除する</div>
-                  <input class="hidden-destroy" type="checkbox" value="${index}" name="product[item_imgs][_destroy]" id="product_item_imgs__destroy">
                 </div>`;
     return html;
   }
@@ -23,12 +25,23 @@ $(function(){
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
   lastIndex = $('.input_box__upload:last').data('index');
   fileIndex.splice(0, lastIndex);
-  $('.hidden-destory').hide();
   
+
+  let resetcunt = $('.input_box__upload').length;
+  if (resetcunt == 1) {
+    console.log("0枚");
+  } else if (resetcunt == 10) {
+    console.log("10数枚");
+    $('.input_box').addClass("resetcunt");
+  } else if (resetcunt < 10) {
+    console.log("数枚");
+    $('.input_box').addClass("resetcunt");
+    $('.showing__images__file').append(nextInput(fileIndex[0]));
+  }
+
 
   $('.showing__images__file').on('change', '.input_box__upload', function(e){
     let targetIndex = $(this).data('index');
-
     let file = e.target.files[0];
     let blobUrl = window.URL.createObjectURL(file);
     let preview_count = $('.previews__image').length;
@@ -36,7 +49,6 @@ $(function(){
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
     } else { 
-    //   console.log(preview_count);
       $('.previews').prepend(previewImages(targetIndex, blobUrl));
       fileIndex.shift();
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
@@ -53,20 +65,14 @@ $(function(){
       }
     }
   });
+
   
   $('.showing__images__file').on('click', '.previews__image__btn', function(){
-
     let targetIndex = $(this).data('btn-index');
-
-    let hiddenCheck = $(`input[value="${targetIndex}"].hidden-destroy`);
-    console.log(hiddenCheck);
-    if (hiddenCheck) hiddenCheck.prop('checked', true);
-
+    $(`div[data-index="${targetIndex}"].input_box__upload`).parent().remove();
     $(this).parent().remove();
-    $(`img[data-image="{index:${targetIndex}}"]`).remove();
-    // $('#product_images_attributes_${targetIndex}_image').remove();
-
-  //   if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+    let hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    hiddenCheck.prop('checked', true)
+    if ($('.input_box').length == 0) $('.showing__images__file').append(nextInput(fileIndex[0]));
   });
-
 });
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,14 +9,15 @@ class ProductsController < ApplicationController
     @product = Product.new
     @product.images.new
     # ↓Sequl Proに直入力したユーザーIDを引っ張り出す記述
+    # ↓ユーザー登録機能実装後は不要
     @user = User.find(1)
   end
 
   def create
     # ↓Sequl Proに直入力したユーザーIDを引っ張り出す記述
+    # ↓ユーザー登録機能実装後は不要
     @user = User.find(1)
     @product = Product.new(product_params)
-    # binding.pry
     if @product.save
       redirect_to root_path
     else
@@ -47,6 +48,7 @@ class ProductsController < ApplicationController
   private
   def product_params
     params.require(:product).permit(:name, :explanation, :category_id, :brand, :product_status, :delivery_cost, :shipping_origin, :delivery_day, :price, images_attributes: [:image, :_destroy, :id]).merge(user_id: @user.id)
+    # ↑ @user.idの部分はユーザー登録機能実装後は<"current_user.id">に変更
   end
 
   def set_product
