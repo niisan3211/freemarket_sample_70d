d430892fe803bcfad29b31a1c14e53ed7fa1f1bf
niisan3211 <kappahaookikunaritai@icloud.com>
2020-07-11 17:25:56 +0900

    自動デプロイの為に２

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,18 +1,24 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
-# app_path = File.expand_path('../../../', __FILE__)
+# app_path = File.expand_path('../../', __FILE__)
+# ↑自動デプロイ前
+app_path = File.expand_path('../../../', __FILE__)
+# ↑自動デプロイ後
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory app_path
-# working_directory "#{app_path}/current"
+# working_directory app_path
+# ↑自動デプロイ前
+working_directory "#{app_path}/current"
+# ↑自動デプロイ後
 
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/tmp/pids/unicorn.pid"
-# pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+# pid "#{app_path}/tmp/pids/unicorn.pid"
+# ↑自動デプロイ前
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+# ↑自動デプロイ後
 
 
 #ポート番号を指定
@@ -21,14 +27,17 @@ listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 # ↑自動デプロイ前用
 
 #エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-# stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+# stderr_path "#{app_path}/log/unicorn.stderr.log"
+# ↑自動デプロイ前
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+# ↑自動デプロイ後
 
 
 #通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
-# stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
-
+# stdout_path "#{app_path}/log/unicorn.stdout.log"
+# ↑自動デプロイ前
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+# ↑自動デプロイ後
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
